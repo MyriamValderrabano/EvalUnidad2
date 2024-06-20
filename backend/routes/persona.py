8c2e04aababa8557d93cc141a9ddd67f7e8d51cb
Myriam <210467@utxicotepec.edu.mx>
2024-06-20 15:16:28 -0600

    Creacion de metodos GET Y POST

--- backend/routes/persona.py
+++ backend/routes/persona.py
@@ -1,6 +1,34 @@
 from fastapi import APIRouter
-persona= APIRouter()
-@persona.get("/personas")
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
-def helloworld():
-    return "Hello world"
+persona= APIRouter()
+personas=[]
+class model_personas(BaseModel):
+    id:int
+    nombre:str
+    primer_apellido: str
+    segundo_apellido:Optional[str] 
+    edad:int
+    fecha_nacimiento:datetime
+    curp:str
+    tipo_sangre:str
+    created_at:datetime = datetime.now()
+    estatus: bool = False
+
+
+@persona.get("/")
+
+def bienvenida():
+    return "Bienvenido a la API del sistema"
+
+@persona.get('/personas')
+
+def get_personas():
+    return personas
+
+@persona.post('/personas')
+def save_personas(datos_persona:model_personas):
+    personas.append(datos_persona)
+    return "Datos guardados correctamente"
